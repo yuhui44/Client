3bb7bdb8574ef9b019faff8761db4ea73737cfcb
yuhui44 <yuhui44@live.cn>
2018-03-27 01:27:36 +0800

    部分引入element

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { Button, Select } from 'element-ui';
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(Button);
+Vue.use(Select);
 
 /* eslint-disable no-new */
 new Vue({
